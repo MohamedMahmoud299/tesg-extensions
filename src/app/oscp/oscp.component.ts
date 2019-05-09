1b3d4668ec17628bce242b06962a2053be33c03e
Mohamed Mahmoud <mohamed@makeitsimple.info>
2019-05-09 22:20:42 +0200

    updates

--- src/app/oscp/oscp.component.ts
+++ src/app/oscp/oscp.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-oscp',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OscpComponent implements OnInit {
 
-  constructor() { }
+  public iframeUrl = 'https://tesg-oscp.s3.amazonaws.com/index.html#/jobs';
+
+  constructor(private route: ActivatedRoute, public domSanitizer: DomSanitizer) {
+  }
 
   ngOnInit() {
+    this.route.queryParamMap.subscribe((params) => {
+      let utmSource = params.get('utm_source');
+      let utmMedium = params.get('utm_medium');
+      let utmCampaign = params.get('utm_campaign');
+      let jobId = params.get('job');
+      let paramsArr = [{
+        key: 'utm_source', value: utmSource
+      }, {
+        key: 'utm_medium', value: utmMedium
+      }, {
+        key: 'utm_campaign', value: utmCampaign
+      }];
+
+      if (jobId) {
+        this.iframeUrl += '/' + jobId;
+      }
+      if (!this.iframeUrl.includes('?') && (utmSource || utmMedium || utmCampaign)) {
+        this.iframeUrl += '?';
+      }
+
+      paramsArr.forEach((item) => {
+        if (item.value) {
+          this.iframeUrl += '&' + item.key + '=' + item.value;
+
+        }
+
+      });
+      console.log('iframurl ', this.iframeUrl);
+    });
   }
 
 }
